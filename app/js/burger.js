9f407ac6d7c8d5d7183b4f78ad659ea6fcc95567
Serhii Shevchenko <greyzerua@ukr.net>
2024-04-02 18:42:45 +0300

    fixed burger menu

--- app/js/burger.js
+++ app/js/burger.js
@@ -1,4 +1,20 @@
-$(".toggle-menu").click(function () {
-  $(this).toggleClass("active");
-  $("#menu").toggleClass("open");
+document.addEventListener("DOMContentLoaded", function () {
+  let toggleMenuButton = document.querySelector(".toggle-menu");
+  let menu = document.getElementById("menu");
+
+  toggleMenuButton.addEventListener("click", function () {
+    if (toggleMenuButton.classList.contains("active")) {
+      toggleMenuButton.classList.remove("active");
+    } else {
+      toggleMenuButton.classList.add("active");
+    }
+
+    if (menu.classList.contains("open")) {
+      document.body.style.overflow = "";
+      menu.classList.remove("open");
+    } else {
+      document.body.style.overflow = "hidden";
+      menu.classList.add("open");
+    }
+  });
 });
